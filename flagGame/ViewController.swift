426e5936155d9aeaf7c7380ccbd9076f52fc2e4b
Andrea Lopez Palomeque <andrealopezpalomeque@gmail.com>
2023-11-15 21:03:42 -0300

    connecting flags with the title and adding an alert with the score

--- flagGame/ViewController.swift
+++ flagGame/ViewController.swift
@@ -15,12 +15,13 @@ class ViewController: UIViewController {
 
     var countries = [String]()
     var score = 0
+     var correctAnswer = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         countries+=["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
-
+    
         button1.layer.borderWidth = 1
         button2.layer.borderWidth = 1
         button3.layer.borderWidth = 1
@@ -34,12 +35,36 @@ class ViewController: UIViewController {
 
     func askQuestion(action: UIAlertAction! = nil) {
         countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+
+
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for:.normal)
+
+        title = countries[correctAnswer].uppercased()
         
     }
 
 
+    @IBAction func buttonTapped(_ sender: UIButton) {
+
+        var title: String
+
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+
+        let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .alert) // create an alert controller
+
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion)) // add an action to the alert controller
+        
+        present(ac, animated: true) // present the alert controller
+    }
+    
 }
 
